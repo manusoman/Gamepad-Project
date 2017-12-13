f64117bd3d4ea0ea6a84c468fa1eb8a7df6cf5de
Manu Soman <manu.naturep@gmail.com>
2017-12-13 15:33:14 +0530

    Add files via upload

--- script/GPInterface.js
+++ script/GPInterface.js
@@ -24,17 +24,69 @@
         handleGamepad: function(e, isConnected) {
             
             if(isConnected) {
+                GPstatus.className = "on";
                 this.GP = e.gamepad;
-                GPstatus.innerHTML = "Gamepad is connected";
                 processLoop();
             } else {
+                GPstatus.className = "off";
                 this.GP = false;
                 flag = false;
-                GPstatus.innerHTML = "Gamepad is disconnected";
             }
             
             console.log(this.GP);
         }
     };
+    
+    
+    
+    
+    // -------------------------------------------------
+    
+    let x,
+        t = 0.5;
+    
+    var axis1 = {
+        
+        h: {
+            pressed: false,
+            timeStamp: 0,
+            duration: 0,
+            value: 0
+        },
+        
+        v: {
+            pressed: false,
+            timeStamp: 0,
+            duration: 0,
+            value: 0
+        }
+    }
+    
+    
+    function check(a, av) {
+        let tmp;
+        av >= t ? tmp = 1 : tmp = 0;
+        av <= -t ? tmp = -1 : tmp = 0;
+        
+        if(tmp) {
+            
+            if(a.pressed) {
+                a.duration = Date.now() - a.timeStamp;
+            } else {
+                a.pressed = true;
+                a.timeStamp = Date.now();
+                a.value = tmp;
+            }
+            
+        } else {
+            
+            if(a.pressed) {
+                a.pressed = false;
+                a.timeStamp = 0;
+                a.duration = 0;
+                a.value = 0;
+            }            
+        }
+    }  
     
 })();
