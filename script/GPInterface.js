4e72f0de5f97a8ac28ed42471f9e3043e5633787
Manu Soman <manu.naturep@gmail.com>
2017-12-14 00:44:31 +0530

    Add files via upload

--- script/GPInterface.js
+++ script/GPInterface.js
@@ -2,91 +2,96 @@
     
     "use strict";
     
-    var GPstatus = document.getElementById("GPStatus"),
-        flag = true,
-        processLoop = function() {
-            stringer.GPInterface.getGPStatus();
-            if(flag) {
-                window.requestAnimationFrame(processLoop);
-            }
+    
+    function Axis() {
+        
+        this.h = {
+            pressed: false,
+            held: false,
+            timeStamp: 0,
+            //duration: 0,
+            value: 0
         };
+        
+        this.v = {
+            pressed: false,
+            held: false,
+            timeStamp: 0,
+            //duration: 0,
+            value: 0
+        };
+    }
+    
+    
+    Axis.prototype = {
+        
+        constructor: Axis,
+        
+        AT: 0.5, // AT -> axis threshold
+        HT: 500, // HT -> button held threshold
+        
+        setAxisStatus: function(vals) {
+            
+            for(let i = 0; i < 2; i++) {
+                
+                let a  = [this.h, this.v][i], // a -> axis
+                    av = vals[i],             // av -> axis value 
+                    tmp;
+
+                tmp = (av >= this.AT) ? 1 : 0;
+                tmp = (av <= -this.AT) ? -1 : 0;
+
+                if(tmp) {
+
+                    if(a.pressed) {
+                        //a.duration = Date.now() - a.timeStamp;
+                        a.held = (Date.now() - a.timeStamp) >= this.HT ? true : false;
+                    } else {
+                        a.pressed = true;
+                        a.timeStamp = Date.now();
+                        a.value = tmp;
+                    }
+
+                } else {
+
+                    if(a.pressed) {
+                        a.pressed = false;
+                        a.held = false;
+                        a.timeStamp = 0;
+                        //a.duration = 0;
+                        a.value = 0;
+                    }            
+                }
+            }
+        }
+    };
+    
     
     stringer.GPInterface = {
         
         GP: {},
         
-        axesThreshold: 0.5,
+        axisL : new Axis(),
+        axisR : new Axis(),
         
-        getGPStatus: function() {
-            //console.log("test");
+        getGPBStatus: function() {
+            
+            this.axisL.setAxisStatus([this.GP.axes[0], this.GP.axes[1]]);
+            this.axisR.setAxisStatus([this.GP.axes[2], this.GP.axes[3]]);
+            
+            this.operateKeypad();
+            
         },
         
-        handleGamepad: function(e, isConnected) {
-            
-            if(isConnected) {
-                GPstatus.className = "on";
-                this.GP = e.gamepad;
-                processLoop();
-            } else {
-                GPstatus.className = "off";
-                this.GP = false;
-                flag = false;
-            }
-            
+        operateKeypad: function() {
+            let x = this.axisL.h.value;
+            stringer.keypadManager.keypadSwitch = 
+        },
+        
+        handleGamepad: function(e, isConnected) {            
+            this.GP = isConnected ? e.gamepad : false;            
             console.log(this.GP);
         }
     };
-    
-    
-    
-    
-    // -------------------------------------------------
-    
-    let x,
-        t = 0.5;
-    
-    var axis1 = {
-        
-        h: {
-            pressed: false,
-            timeStamp: 0,
-            duration: 0,
-            value: 0
-        },
-        
-        v: {
-            pressed: false,
-            timeStamp: 0,
-            duration: 0,
-            value: 0
-        }
-    }
-    
-    
-    function check(a, av) {
-        let tmp;
-        av >= t ? tmp = 1 : tmp = 0;
-        av <= -t ? tmp = -1 : tmp = 0;
-        
-        if(tmp) {
-            
-            if(a.pressed) {
-                a.duration = Date.now() - a.timeStamp;
-            } else {
-                a.pressed = true;
-                a.timeStamp = Date.now();
-                a.value = tmp;
-            }
-            
-        } else {
-            
-            if(a.pressed) {
-                a.pressed = false;
-                a.timeStamp = 0;
-                a.duration = 0;
-                a.value = 0;
-            }            
-        }
-    }  
     
 })();
--- script/init.js
+++ script/init.js
@@ -1,24 +1,29 @@
-(function() {
+(function(GPI) {
     
     "use strict";
     
-    stringer.keypadManager.KTLength = stringer.keypadManager.keypadTypes.length;
-    stringer.keypadManager.activeKeypad = stringer.keypadManager.keypadTypes[0];
+    var GPstatus = document.getElementById("GPStatus");
     
+    function processLoop() {            
+        GPI.getGPBStatus();
+        if(GPI.GP) window.requestAnimationFrame(processLoop);
+    }
+    
+    stringer.keypadManager.KTLength = stringer.keypadManager.keypadTypes.length;
+    stringer.keypadManager.activeKeypad = stringer.keypadManager.keypadTypes[0];    
     
     window.addEventListener("gamepadconnected", function(e) {
-        stringer.GPInterface.handleGamepad(e, true);
+        GPI.handleGamepad(e, true);
+        GPstatus.className = "on";
+        processLoop();
     });
     
     window.addEventListener("gamepaddisconnected", function(e) {
-        stringer.GPInterface.handleGamepad(e, false);
+        GPI.handleGamepad(e, false);
+        GPstatus.className = "off";
     });
     
-    window.addEventListener("gamepadaxismove", function(e){
-       console.log("axis moved!");
-    });
-    
-})();
+})(window.stringer.GPInterface);
 
 /*(function() { // Automated Sample
     
